430e0ad41ee12c4cd2de806120908fae31439a51
25mx349 <25mx349@MCA.PSG>
2026-07-15 11:02:26 +0530

    Added calculator functions

---
+++ src/calculator.py
@@ -0,0 +1,23 @@
+def add(a, b):
+    return a + b
+
+
+def subtract(a, b):
+    return a - b
+
+
+def multiply(a, b):
+    return a * b
+
+
+def divide(a, b):
+    if b != 0:
+        return a / b
+    else:
+        return "Cannot divide by zero"
+
+
+print("Addition:", add(10, 5))
+print("Subtraction:", subtract(10, 5))
+print("Multiplication:", multiply(10, 5))
+print("Division:", divide(10, 5))
